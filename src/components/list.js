4ea5be6c5766cb0d42db6ba6ef809e8980356cb1
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-05-02 13:30:32 -0700

    Zip search lists and add phone number

--- src/components/list.js
+++ src/components/list.js
@@ -1,14 +1,22 @@
 import Image from "next/image";
 
 function list({ reset, list1, list2 }) {
-  console.log("list1", list1);
-  console.log("list2", list2);
+  const list = (() => {
+    const combinedList = [];
+    let idx = 0;
+    while (list1.length > idx && list2.length > idx) {
+      combinedList.push(list1[idx]);
+      combinedList.push(list2[idx]);
+      idx++;
+    }
+    return [...combinedList, ...list1.slice(idx), ...list2.slice(idx)];
+  })();
+
   return (
     <section>
       <div className="flex flex-col gap-5">
-        {list1 &&
-          list2 &&
-          [...list1, ...list2].map((item, i) => (
+        {list.length > 0 &&
+          list.map((item, i) => (
             <div key={i}>
               <h2>{item.name}</h2>
               <Image src={item.image_url} alt={item.name} width={200} height={200} />
@@ -18,6 +26,7 @@ function list({ reset, list1, list2 }) {
                   <p key={i}>{line}</p>
                 ))}
               </div>
+              <p>Phone: {item.display_phone}</p>
               <p>Price: {item.price}</p>
               <p>Rating: {item.rating}</p>
               <a href={item.url}>Find on Yelp</a>
