9fe62b7cccb0e4b47b201b1fba2670c298867cd8
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-05-01 18:26:55 -0700

    Remove excess Head

--- src/pages/compromise.js
+++ src/pages/compromise.js
@@ -2,17 +2,9 @@ import Head from "next/head";
 
 export default function Compromise() {
   return (
-    <>
-      <Head>
-        <title>Yelp for Couples</title>
-        <meta name="description" content="Yelp for Couples" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1 className="text-4xl font-bold text-center">Yelp for Couples</h1>
-        <p>Compromise Page</p>
-      </main>
-    </>
+    <main>
+      <h1 className="text-4xl font-bold text-center">Yelp for Couples</h1>
+      <p>Compromise Page</p>
+    </main>
   );
 }
