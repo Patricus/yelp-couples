1b04b2c9088329b094a77963b28942a9af705ed6
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-05-03 10:21:28 -0700

    Search styling

--- src/pages/compromise.js
+++ src/pages/compromise.js
@@ -14,22 +14,16 @@ export default function Compromise() {
   };
 
   return (
-    <main>
-      <h1 className="text-4xl  text-center bg-fuchsia-300 py-2 uppercase">
-        <span className="font-bold text-cyan-500">Y</span>elp{" "}
-        <span className="font-bold text-cyan-500">f</span>or{" "}
-        <span className="font-bold text-cyan-500">c</span>ouples
+    <main className="flex flex-col items-center">
+      <h1 className="text-4xl  text-center bg-fuchsia-300 py-2 uppercase mb-10 w-screen">
+        <span className="font-bold text-cyan-500">D</span>uo
+        <span className="font-bold text-cyan-500">D</span>ine
       </h1>
-      <p>Compromise Page</p>
 
       {hasSearched ? (
         <List reset={reset} list1={list1} list2={list2} />
       ) : (
-        <Search
-          setHasSearched={setHasSearched}
-          setList1={setList1}
-          setList2={setList2}
-        />
+        <Search setHasSearched={setHasSearched} setList1={setList1} setList2={setList2} />
       )}
     </main>
   );
